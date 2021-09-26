43bd4435593dd8d2adf4d0236c6a06765c225b5d
Rhaegar09 <edwardreinahortua@gmail.com>
2021-09-25 22:52:27 -0500

    Cambios casi finales

--- App/app.js
+++ App/app.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
-import { getDatabase, ref, onValue, set } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-database.js";
+import { getDatabase, ref, onValue, set, child, get } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-database.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,50 +18,62 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-//Escritura de datos 
-function enviarDatos (alarma, aireAcondicionado) {
-    const alarm = alarma;
-    const aire = aireAcondicionado;
-    set(ref(db, 'Sensores/'), {
-        Alarma: alarm,
-        Control: aire,
-    });
-}
-
 // Lectura temperatura
-const temperatura = ref(db, 'Sensores/' + '/Temperatura');
-onValue(temperatura, (snapshot) => {
-    const datosTemp = snapshot.val();
-    const temp = document.getElementById('temp');
-    temp.innerText = datosTemp + ' °C';
-    console.log("Temperatura:",datosTemp);
+setInterval(() => {
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `Sensores/${"Temperatura"}`)).then((snapshot) => {
+        if (snapshot.exists()) {
+            const datosTemp = snapshot.val();
+            //console.log("Temperatura get: ",datosTemp);
+            const temp = document.getElementById('temp');
+            temp.innerText = datosTemp + ' °C';
+            console.log("Temperatura:",datosTemp);
 
-    const tempMax = document.getElementById('tempMax');
-    const valTempMax = tempMax.value;
-    const tempMin = document.getElementById('tempMin');
-    const valTempMin = tempMin.value;
-    const termometro = document.getElementById('termometro');
+            const tempMax = document.getElementById('tempMax');
+            const valTempMax = tempMax.value;
+            const tempMin = document.getElementById('tempMin');
+            const valTempMin = tempMin.value;
+            const termometro = document.getElementById('termometro');
+            const estado = document.getElementById('estado');
+            const imEstado = document.getElementById('imEstado');
+            const aire = document.getElementById('aire');
+            const imAire = document.getElementById('imAire');
+            
+            if (datosTemp > valTempMax) {
+                estado.innerText = "Temperatura alta";
+                imEstado.src = "./imag/calor.png";
+                imAire.src = "./imag/aire.png";
+                aire.innerText = "Aire acondicionado encendido";
+                termometro.classList.remove("fa-thermometer-empty");
+                termometro.classList.remove("fa-thermometer-half");
+                termometro.classList.add("fa-thermometer-full");
+                //enviarDatos(2,1);
+            } else if (datosTemp < valTempMin) {
+                estado.innerText = "Temperatura baja";
+                imEstado.src = "./imag/frio.png";
+                imAire.src.
+                aire.innerText = "Aire acondicionado apagado";
+                termometro.classList.remove("fa-thermometer-full");
+                termometro.classList.remove("fa-thermometer-half");
+                termometro.classList.add("fa-thermometer-empty");
+                //enviarDatos(4,0);
+            } else {
+                estado.innerText = "Temperatura normal";
+                imEstado.src = "./imag/normal.png";
+                aire.innerText = "Aire acondicionado apagado";
+                termometro.classList.remove("fa-thermometer-full");
+                termometro.classList.remove("fa-thermometer-empty");
+                termometro.classList.add("fa-thermometer-half");
+                //enviarDatos(3,0);
+            }
+        } else {
+        console.log("No data available");
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
+} , 1000);
 
-    if (datosTemp > valTempMax) {
-        estado.innerText = "Temperatura alta!!";
-        termometro.classList.remove("fa-thermometer-empty");
-        termometro.classList.remove("fa-thermometer-half");
-        termometro.classList.add("fa-thermometer-full");
-        //enviarDatos(2,1);
-    } else if (datosTemp < valTempMin) {
-        estado.innerText = "Temperatura baja";
-        termometro.classList.remove("fa-thermometer-full");
-        termometro.classList.remove("fa-thermometer-half");
-        termometro.classList.add("fa-thermometer-empty");
-        //enviarDatos(4,0);
-    } else {
-        estado.innerText = "Temperatura normal";
-        termometro.classList.remove("fa-thermometer-full");
-        termometro.classList.remove("fa-thermometer-empty");
-        termometro.classList.add("fa-thermometer-half");
-        //enviarDatos(3,0);
-    }
-});
 /*
 fa-thermometer-empty – Vacío
 fa-thermometer-quarter – Un cuarto
@@ -70,13 +82,21 @@ fa-thermometer-three-quarters – Tres cuartos
 fa-thermometer-full – Lleno
 */
 // Lectura humedad
-const humedad = ref(db, 'Sensores/' + '/Humedad');
-onValue(humedad, (snapshot) => {
-    const data = snapshot.val();
-    const hum = document.getElementById('hum');
-    hum.innerText = data + " %";
-    console.log("Humedad:",data);
-});
+setInterval(() => { 
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `Sensores/${"Humedad"}`)).then((snapshot) => {
+        if (snapshot.exists()) {
+            const datosHum = snapshot.val();
+            const hum = document.getElementById('hum');
+            hum.innerText = datosHum + " %";
+            console.log("Humedad:",datosHum);
+        } else {
+            console.log("No data available");
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
+} , 1000);
 
 
 // Sliders temperatura
@@ -96,23 +116,31 @@ sliderMin.oninput = function () {
     minima.textContent = this.value;
 }
 
-// Escritura de datos
-/*const alarm = 1;
-const aire = 2;
-set(ref(db, 'Sensores/'), {
-    Alarma: alarm,
-    Control: aire,
-});
-*/
-// Estado alarma
-//const alarma = ref(db, 'Sensores/' + '/Alarma');
-//onValue(alarma, (snapshot) => {
-    //const data = snapshot.val();
-    //const alarm = document.getElementById('alarm');
-    //alarm.innerText = data;
-    //console.log("Alarma:",data);
-//});
+//Escritura de datos 
+function enviarDatos (alarma, aireAcondicionado) {
+    const alarm = alarma;
+    const aire = aireAcondicionado;
+    set(ref(db, 'Sensores/'), {
+        Alarma: alarm,
+        Control: aire,
+    });
+}
 
+setInterval(() => {
+    const termometro = document.getElementById('termometro');
+    const lleno = termometro.classList.contains("fa-thermometer-full");
+    //const mitad = termometro.classList.contains("fa-thermometer-half");
+    const vacio = termometro.classList.contains("fa-thermometer-empty");
+
+    if (lleno === true) {
+        enviarDatos(2,1);
+    
+    } else if (vacio === true) {
+        enviarDatos(4,0);
+    } else {
+        enviarDatos(3,0);
+    }
+},2500);
 //2 : Temperatura alta
 //3 : Temperatura normal
 //4 : Temperatura baja
--- js/index.js
+++ js/index.js
@@ -77,10 +77,9 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
     
-    if (username === "user" && password === "user") {
+    if (username === "admin" && password === "admin") {
         alert("You have successfully logged in.");
         location.href = "./App/app.html";
-        //location.reload();
     } else {
         loginErrorMsg.style.opacity = 1;
     }
